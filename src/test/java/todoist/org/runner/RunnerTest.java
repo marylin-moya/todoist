f420337d4da3ae9bd00527cb8938f565a976ed8a
marylin moya <marylin.moya@jalasoft.com>
2021-03-24 22:28:06 -0400

    RunnerTest

--- src/test/java/todoist/org/runner/RunnerTest.java
+++ src/test/java/todoist/org/runner/RunnerTest.java
@@ -1,4 +1,4 @@
-package todoist.org.steps;
+package todoist.org.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
@@ -6,17 +6,16 @@ import org.apache.log4j.Logger;
 import org.testng.annotations.AfterTest;
 import selenium.DriverManager;
 
-@CucumberOptions(
-        plugin = {"pretty", "html:build/cucumber/cucumber-report.html",
-        "json:build/cucumber/cucumber-report.json", "rerun:build/cucumber/rerun.txt" },
-        glue = {"todoist.org"},
-        features = {"classpath:features/login","classpath:features/projects"}, tags = "@Login")
+//@CucumberOptions(
+//        plugin = {"pretty", "json:build/cucumber/cucumber-report.json"},
+//        glue = {"todoist.org"},
+//        features = {"classpath:features/login"})
 public class RunnerTest extends AbstractTestNGCucumberTests {
     private static Logger log = Logger.getLogger("RunnerTest");
 
     @AfterTest
     public void afterExecution(){
         log.info("-------------------After Execution---------------");
-        DriverManager.getInstance().quitDriver();
+
     }
 }
